c0dbfba9f92ef765719ce280112eedbc2d050e3d
Champchips <otakuchamp@gmail.com>
2021-04-09 23:57:18 +0700

    Add env

---
+++ code/backend/src/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+
+import './mongoose-connect'
+import schema from './graphql'
+
+const server = new ApolloServer({
+  schema,
+  // introspection: true,
+  playground: true,
+})
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+server.applyMiddleware({ app })
+
+const port = process.env.PORT ?? 5001
+app.listen({ port }, () => {
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+})
