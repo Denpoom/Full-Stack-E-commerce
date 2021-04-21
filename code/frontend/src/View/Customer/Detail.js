f41378700f730a7bfee98ef08bafea1c7590b540
Core2931 <mark.2631@hotmail.com>
2021-04-21 17:58:12 +0700

    " test"

--- code/frontend/src/View/Customer/Detail.js
+++ code/frontend/src/View/Customer/Detail.js
@@ -7,10 +7,6 @@ const Detail = () => {
         <React.Fragment >
             <Suspense fallback={<h1>Still Loading…</h1>}>  
                 <DetailProduct/>
-<<<<<<< HEAD
-=======
-               
->>>>>>> cbeb370307546724608957ed405ab17c1c5f8ba0
             </Suspense>
         </React.Fragment>
     )
