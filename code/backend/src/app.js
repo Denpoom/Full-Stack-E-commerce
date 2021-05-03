371c82d4cca646172711f7624f1895ff653af2e3
Denpoom Srimuang <forizzle28@hotmail.com>
2021-05-03 13:39:43 +0700

    add heroku

--- code/backend/src/app.js
+++ code/backend/src/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: process.env.CORS ?? "http://localhost:3000",
+    origin: true,
     credentials: true,
   })
 );
@@ -57,7 +57,7 @@ server.applyMiddleware({
   app,
   path,
   cors: {
-    origin: process.env.CORS ?? "http://localhost:3000",
+    origin: true,
     credentials: true,
   },
 });
